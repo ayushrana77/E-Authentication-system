1864e623bc4f3d79f36a95df6c9701da74024f77
Ayush rana <ayushrana200177@gmail.com>
2022-05-03 11:23:52 +0530

    add page

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import {Routes,Route} from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
