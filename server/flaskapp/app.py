eed734383e52583cdd97ca669ba1463e5414f2c8
Ayush rana <ayushrana200177@gmail.com>
2022-05-06 21:23:40 +0530

    add the register api.server and insert the data in to the mongodb

--- server/flaskapp/app.py
+++ server/flaskapp/app.py
@@ -22,5 +22,43 @@ app.config['JWT_SECRET_KEY'] = 'this-is-secert-key'
 def hello_world():
     return "Hello"
 
+@app.route("/adminRegister",methods=['POST'])
+def adminRegister():
+    allusers = mongo.db.admins
+
+    user = allusers.find_one({'email':request.json['email']})
+    phone = allusers.find_one({'phone':request.json['phone']})
+
+    if user:
+        return jsonify(message='Email already exists'),401
+    if phone:
+        return jsonify(message='phone number alredy exists'),401
+
+    if request.json['password'] != request.json['cpassword']:
+        return jsonify(message='password not Matching!!!'),401
+    
+    hashpw = bcrypt.hashpw(
+        request.json['password'].encode('utf-8'),bcrypt.gensalt()
+    )
+    hashcpw = bcrypt.hashpw(
+        request.json['cpassword'].encode('utf-8'),bcrypt.gensalt()
+    )
+
+    access_token = create_access_token(identity=request.json['email'])
+
+    allusers.insert_one({
+        'email':request.json['email'],
+        'name':request.json['name'],
+        'phone':request.json['phone'],
+        'password':hashpw,
+        'cpassword':hashcpw,
+        'tokens':[{
+            'token':str(access_token)
+        }]
+    })
+
+    return jsonify(token = str(access_token)),200
+
+
 if __name__ == '__main__':
     app.run(debug=True)
